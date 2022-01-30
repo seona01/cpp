de5d97c46f4bddb12b7b7d1430b50ca0754427f5
Seona Yang <n2nnn2n@gmail.com>
2022-01-31 00:50:43 +0900

    second commit

--- 04_functionTemplate/04_functionTemplate/SwapFTMain.cpp
+++ 04_functionTemplate/04_functionTemplate/SwapFTMain.cpp
@@ -13,7 +13,7 @@ int main()
 
 	MyString s1("KNOU"), s2("CS");
 	cout << "s1 = " << s1 << ", s2 = " << s2 << endl;
-	swapFT(s1, s2);										// 사용자 정의 클래스 MyString객체를 함수템플릿으로 전달하여 호출. 사용된 인수의 자료형에 해당하는 swapFT() 함수가 자동으로 정으되어 컴파일
+	swapFT(s1, s2);										// 사용자 정의 클래스 MyString객체를 함수템플릿으로 전달하여 호출. 사용된 인수의 자료형에 해당하는 swapFT() 함수가 자동으로 정의되어 컴파일
 	cout << "값 교환 후 --> ";
 	cout << "s1 = " << s1 << ", s2 = " << s2 << endl;
 	return 0;
